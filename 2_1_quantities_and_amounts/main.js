3783f7f7e0caf59f3e1295ed8226b44a1ea2e6df
Betsy Rosalen <betsy@mylittleuniverse.com>
2022-02-16 21:14:17 -0500

    Completed class activity tutorial 2.1
    Completed class activity tutorial 2.1

--- 2_1_quantities_and_amounts/main.js
+++ 2_1_quantities_and_amounts/main.js
@@ -3,15 +3,51 @@
 // const height = ;
 
 /* LOAD DATA */
-d3.csv('../[PATH_TO_YOUR_DATA]', d3.autoType)
-  .then(data => {
-    console.log("data", data)
 
-    /* SCALES */
-    /** This is where you should define your scales from data to pixel space */
+// d3.csv('../[PATH_TO_YOUR_DATA]', d3.autoType)
+//   .then(data => {
+//     console.log("data", data)
+
+//     /* SCALES */
+//     /** This is where you should define your scales from data to pixel space */
     
 
-    /* HTML ELEMENTS */
-    /** Select your container and append the visual elements to it */
+//     /* HTML ELEMENTS */
+//     /** Select your container and append the visual elements to it */
+
+//   }) 
+
+
+
+  
+  // 1 VARIABLES
+
+  const width = window.innerWidth *.08;
+  const height = window.innerHeight /3;
+
+  // 2 DATA
+
+  d3.csv('../data/squirrelActivities.csv', d3.autoType).then(data=> {
+    console.log("data", data)
+  
+
+  // 3 SCALES
+
+  const xSCale = d3.scaleBand()
+  .domain(data.map(d=> d.activity))
+  .range([0, width])
+
+  const yScale = d3.scaleLinear()
+  .domain([0, d3.max(data, d=> d.count)]) // domain relates to data
+  .range([height, 0]) // range relates to visuals
+
+  // 4 ELEMENTS APPEND
+
+
+  // 5 SELECT - JOIN - DRAW
+
+
+
+  // 6 ATTTRIBUTES
 
   })
