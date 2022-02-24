81e888c62221a501de1813ec5922b7e06ab1499a
Betsy Rosalen <betsy@mylittleuniverse.com>
2022-02-24 00:33:51 -0500

    Tutorial 1.3 completed

--- 1_3_intro_to_js/main.js
+++ 1_3_intro_to_js/main.js
@@ -1,1 +1,15 @@
 console.log('hello world');
+
+const label = document.getElementById("number")
+const button = document.getElementById("counter")
+let count = 0
+
+function countUpdate(){
+    count+=1;
+    if (count == 1) {
+        label.innerText = "Ah hah hah hah!  You have clicked " + count + " time!";
+    } else {
+        label.innerText = "Ah hah hah hah!  You have clicked " + count + " times!"
+    }
+    button.innerText = "Count one more?"
+}
