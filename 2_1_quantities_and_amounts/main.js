379062b3d13b1c1205a955c44326bf5f771cf0cb
Betsy Rosalen <betsy@mylittleuniverse.com>
2022-04-03 17:18:06 -0400

    Much cleaner code now!

--- 2_1_quantities_and_amounts/main.js
+++ 2_1_quantities_and_amounts/main.js
@@ -6,41 +6,54 @@ const margin = {top: 20, right: 30, bottom: 80, left: 60};
 /* LOAD DATA */
 d3.csv('../data/squirrelActivities.csv', d3.autoType)
 .then(data => {
-  console.log("data", data)
+  console.log("data", data);
 
   /* SCALES */
+
   // xscale - linear, count 
   const xScale = d3.scaleLinear()
     .domain([0, d3.max(data, d => d.count)])  //data
-    .range([0, width-margin.right])  //svg
+    .range([margin.left, width-margin.right]); //svg
 
   // yscale - categorical, activity
   const yScale = d3.scaleBand()
     .domain(data.map(d => d.activity))
     .range([margin.top, height-margin.bottom]) // visual variable
-    .paddingInner(.15)
+    .paddingInner(.15);
+
+  /* AXES */
+
+  const xAxis = d3.axisBottom(xScale);
+  const yAxis = d3.axisLeft(yScale);
 
   /* HTML ELEMENTS */
+
   // svg
   const svg = d3.select("#container")
     .append("svg")
       .attr("width", width)
-      .attr("height", height)
-    .append("g")
-      .attr("transform", `translate(${margin.left},${margin.top})`)
-      ;
-  svg.append("g")
-    .call(d3.axisBottom(xScale))
-      .attr("transform", `translate(0,${height-margin.bottom +10})`)
-  svg.append("g")
+      .attr("height", height);
+
+  const xAxisGroup = svg.append("g")
+    .attr("class", "xAxis")
+    .attr("transform", `translate(0,${height-margin.bottom +10})`)
+    .call(xAxis);
+
+  const yAxisGroup = svg.append("g")
+    .attr("class", "yAxis")
+    .attr("transform", `translate(${margin.left}, 0)`)
+    .call(yAxis);
+
+
+  /*     svg.append("g")
     .call(d3.axisLeft(yScale))
-    /*     .attr("transform", "translate(0," + height + ")") */
+    .attr("transform", "translate(0," + height + ")") */
 
   // bars
   svg.selectAll("rect")
     .data(data)
     .join("rect")
-    .attr("x", d => 0)
+    .attr("x", margin.left)
     .attr("y", d => yScale(d.activity))
     .attr("width", d => xScale(d.count))
     .attr("height", yScale.bandwidth())
